a38801c18590dc215a795f921a94c2e562cf1baf
Andrea Cui <cuihao0407@gmail.com>
2022-02-27 16:02:43 +0900

    Create test4.py

---
+++ 前期课程/test4.py
@@ -0,0 +1,16 @@
+# coding = utf-8
+
+"""
+you need python
+1，分别得到三个单词
+2，从左到右顺序，隔一取一
+3，从右到左，隔一取一
+4，字符串反序
+"""
+
+a = 'you need python'
+
+print(a[0:3], "\n", a[4:8], '\n', a[-6:])
+print(a[::2])
+print(a[::-2])
+print(a[::-1])
